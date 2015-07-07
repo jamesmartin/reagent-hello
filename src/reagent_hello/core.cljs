89c1015bacf9bf18327a7fe563d23eb5e390a0a4
James Martin <james@roomz.com>
2015-07-07 20:59:56 +1000

    Everything up to and including the color-changing clock

--- src/reagent_hello/core.cljs
+++ src/reagent_hello/core.cljs
@@ -101,6 +101,35 @@
            [:span {:style {:color color}} diagnose]
            [slider :bmi bmi 10 50]]]))
 
+(defonce timer (atom (js/Date.)))
+
+(defonce time-color (atom "#f34"))
+
+(defonce time-updater (js/setInterval
+                        #(reset! timer (js/Date.)) 1000))
+
+(defn greeting [message]
+  [:h1 message])
+
+(defn clock []
+  (let [time-str (-> @timer .toTimeString (clojure.string/split " " ) first)]
+   [:div.example-clock
+    {:style {:color @time-color}}
+    time-str]))
+
+(defn color-input []
+  [:div.color-input
+   "Time color: "
+   [:input {:type "text"
+            :value @time-color
+            :on-change #(reset! time-color (-> % .-target .-value))}]])
+
+(defn simple-example []
+  [:div
+   [greeting "Hello world, it is now"]
+   [clock]
+   [color-input]])
+
 (defn simple-parent []
   [:div
    [:p "I include simple-component."]
@@ -112,11 +141,11 @@
    [counting-component]
    [timer-component]
    [shared-state]
-   [bmi-component]])
+   [bmi-component]
+   [simple-example]])
 
 (reagent/render-component [simple-parent]
                           (. js/document (getElementById "app")))
-
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
